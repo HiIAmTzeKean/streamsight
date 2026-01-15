Fix metric calculation

src/streamsight/matrix/interaction_matrix.py
@@ -3,14 +3,13 @@ import operator
 from collections.abc import Callable
 from copy import deepcopy
 from enum import StrEnum
-from typing import Literal, Self, overload
+from typing import Literal, overload
 from warnings import warn
 
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
 
-from streamsight.utils import to_binary
 from .exception import TimestampAttributeMissingError
 
 
@@ -614,6 +613,15 @@ class InteractionMatrix:
 
         sparse_matrix = csr_matrix((values, indices), shape=shape, dtype=values.dtype)
         return sparse_matrix
+
+    @property
+    def user_id_sequence_array(self) -> np.ndarray:
+        """Array of user IDs in the order of interactions.
+
+        :return: Numpy array of user IDs.
+        :rtype: np.ndarray
+        """
+        return self._df[InteractionMatrix.USER_IX].to_numpy()
 
     def get_prediction_data(self) -> "InteractionMatrix":
         """Get the data to be predicted.

src/streamsight/metrics/ranking/dcg.py
@@ -10,8 +10,8 @@ import logging
 import numpy as np
 from scipy.sparse import csr_matrix
 
-from streamsight.metrics.base import ListwiseMetricK
-from streamsight.metrics.util import sparse_divide_nonzero
+from ..core.listwise_top_k import ListwiseMetricK
+from ..core.util import sparse_divide_nonzero
 
 
 logger = logging.getLogger(__name__)
@@ -37,10 +37,9 @@ class DCGK(ListwiseMetricK):
     IS_BASE: bool = False
 
     def _calculate(self, y_true: csr_matrix, y_pred: csr_matrix) -> None:
-        # log number of users and ground truth interactions
-        logger.debug(f"DCGK compute started - {self.name}")
-        logger.debug(f"Number of users: {y_true.shape[0]}")
-        logger.debug(f"Number of ground truth interactions: {y_true.nnz}")
+        logger.debug("Precision compute started - %s", self.name)
+        logger.debug("Shape of matrix: (%d, %d)", y_true.shape[0], y_true.shape[1])
+        logger.debug("Number of ground truth interactions: %d", y_true.nnz)
 
         denominator = y_pred.multiply(y_true)
         # Denominator: log2(rank_i + 1)

src/streamsight/metrics/ranking/ndcg.py
@@ -7,12 +7,13 @@
 
 import logging
 from typing import Optional
-import numpy as np
 
+import numpy as np
 from scipy.sparse import csr_matrix
 
-from streamsight.metrics.base import ListwiseMetricK
-from streamsight.metrics.util import sparse_divide_nonzero
+from ..core.listwise_top_k import ListwiseMetricK
+from ..core.util import sparse_divide_nonzero
+
 
 logger = logging.getLogger(__name__)
 
@@ -45,20 +46,17 @@ class NDCGK(ListwiseMetricK):
     """
     IS_BASE: bool = False
 
-    def __init__(self, K:Optional[int] = 10, timestamp_limit: Optional[int] = None):
-        super().__init__(K=K, timestamp_limit=timestamp_limit)
-
-        self.discount_template = 1.0 / np.log2(np.arange(2, K + 2))
-        # Calculate IDCG values by creating a list of partial sums
-        self.IDCG_cache = np.concatenate([[1], np.cumsum(self.discount_template)])
-
-    def _calculate(self, y_true: csr_matrix, y_pred_top_K: csr_matrix) -> None:
+    def _calculate(self, y_true: csr_matrix, y_pred: csr_matrix) -> None:
         logger.debug(f"NDCGK compute started - {self.name}")
         logger.debug(f"Number of users: {y_true.shape[0]}")
         logger.debug(f"Number of ground truth interactions: {y_true.nnz}")
 
+        self.discount_template = 1.0 / np.log2(np.arange(2, self.K + 2))
+        # Calculate IDCG values by creating a list of partial sums
+        self.IDCG_cache = np.concatenate([[1], np.cumsum(self.discount_template)])
+
         # Correct predictions only
-        denominator = y_pred_top_K.multiply(y_true)
+        denominator = y_pred.multiply(y_true)
         # Denominator: log2(rank_i + 1)
         denominator.data = np.log2(denominator.data + 1)
         # Binary relevance
@@ -78,5 +76,3 @@ class NDCGK(ListwiseMetricK):
         )
 
         logger.debug(f"NDCGK compute complete - {self.name}")
-
-

src/streamsight/metrics/ranking/recall.py
@@ -3,7 +3,7 @@ import logging
 import scipy.sparse
 from scipy.sparse import csr_matrix
 
-from streamsight.metrics.base import ListwiseMetricK
+from ..core.listwise_top_k import ListwiseMetricK
 
 
 logger = logging.getLogger(__name__)
@@ -29,10 +29,9 @@ class RecallK(ListwiseMetricK):
     def _calculate(self, y_true: csr_matrix, y_pred: csr_matrix) -> None:
         scores = scipy.sparse.lil_matrix(y_pred.shape)
 
-        # log number of users and ground truth interactions
-        logger.debug(f"Recall compute started - {self.name}")
-        logger.debug(f"Number of users: {y_true.shape[0]}")
-        logger.debug(f"Number of ground truth interactions: {y_true.nnz}")
+        logger.debug("Precision compute started - %s", self.name)
+        logger.debug("Shape of matrix: (%d, %d)", y_true.shape[0], y_true.shape[1])
+        logger.debug("Number of ground truth interactions: %d", y_true.nnz)
 
         # obtain true positives
         scores[y_pred.multiply(y_true).astype(bool)] = 1